b49e07d5a28cd7121a8d66f2843edbaae5341a8e
Tamin Mahboubi <taminmahboubi@DESKTOP-FMC8PN8.localdomain>
2026-02-11 10:51:02 +0000

    feat: add file detection logic and nullglob (ghost file) fix

--- txt2dir.sh
+++ txt2dir.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+shopt -s nullglob # stops bash from returning the string '*.txt' if no files exist
+
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
 BOLD="\033[1m"
 RESET="\033[0m"
+
+
+files=( *.txt )
+avbl_files="${#files[@]}"
+
+check_files() {
+	if [ "$avbl_files" -eq 0 ]; then
+		echo -e "${RED}SEARCH FAILED:${RESET} No .txt files were found!"
+	else
+		echo -e "${GREEN}SUCCESS:${RESET} [$avbl_files] .txt files were found!"
+	fi
+}
+
+
+
+
+
 
 echo -e "${BOLD}Select an option:${RESET}"
 
@@ -24,11 +46,10 @@ fi
 case $choice in 
 	"Create folders from .txt file")
 		clear
-		echo "Creating from file..."
+		check_files
 		;;
 	"Create folders dynamically")
 		clear
-		echo "creating dynamically..."
 		;;
 	"Exit")
 		clear
